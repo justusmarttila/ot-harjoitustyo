0f15e6dc9b5ca818acb065ed64d4fca3907fb68d
justusmarttila <marttilajustus2001@gmail.com>
2022-05-03 18:05:25 +0300

    bug

--- minesweeper-game/src/config.py
+++ minesweeper-game/src/config.py
@@ -3,7 +3,7 @@ import os
 dirname = os.path.dirname(__file__)
 
 SCORES_FILENAME = os.getenv("SCORES_FILENAME") or "scores.csv"
-SCORES_FILE_PATH = os.path.join(dirname "..", "data", SCORES_FILENAME)
+SCORES_FILE_PATH = os.path.join(dirname, "..", "data", SCORES_FILENAME)
 
 DATABASE_FILENAME = os.getenv("DATABASE_FILENAME") or "database.sqlite"
 DATABASE_FILE_PATH = os.path.join(dirname, "..", "data", DATABASE_FILENAME)
