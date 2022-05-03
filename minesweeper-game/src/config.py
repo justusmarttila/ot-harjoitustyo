29c6a4299d8f441292032e74650d2146fab681ea
justusmarttila <marttilajustus2001@gmail.com>
2022-05-03 18:27:27 +0300

    config error muutoksia

--- minesweeper-game/src/config.py
+++ minesweeper-game/src/config.py
@@ -1,6 +1,12 @@
 import os
+from dotenv import load_dotenv
 
 dirname = os.path.dirname(__file__)
+
+try:
+    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
+except FileNotFoundError:
+    pass
 
 SCORES_FILENAME = os.getenv("SCORES_FILENAME") or "scores.csv"
 SCORES_FILE_PATH = os.path.join(dirname, "..", "data", SCORES_FILENAME)
